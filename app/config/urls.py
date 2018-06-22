035e3596ca33c16c22f35b23cc8802bd27fa8ec6
Ryanden <lockstom@gmail.com>
2018-06-22 16:25:41 +0900

    login view 구현

--- app/config/urls.py
+++ app/config/urls.py
@@ -22,6 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
+    path('members/', include('members.urls')),
     path('', views.index),
     # path('media/<str:path>', admin.site.urls),
 ] + static(
---
+++ app/members/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def login_view(request):
+    # 1. members.urls <- members 로 include 되도록 config url 에 추가
+    # 2. path 구현
+    # 3. path 와 view 연결
+    # 4. form 작성
+    # 5. post 요청을 보내 뷰에서 잘 왔는지 확인
+    # URL 'members/login/
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            print('성공')
+
+            login(request, user)
+
+            return redirect('posts:post-list')
+        else:
+            print('실패')
+            return redirect('members:login')
+
+    return render(request, 'members/login.html')
+
