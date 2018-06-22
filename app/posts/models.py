dcd19539a8b955cdb31894ee139c02538ddda6f1
Ryanden <lockstom@gmail.com>
2018-06-22 11:52:38 +0900

    Post 클래스 추가, MEDIA_URL 연결
    MEDIA_ROOT는 실제 파일이 업로드되는 기준 경로
    MEDIA_URL은 사용자가 업로드한 파일을 제공할 URL
    config.urls에 추가한 static()은 MEDIA_URL로의 request에대해
    MEDIA_ROOT에서 찾은 파일을 response로 돌려줌.

---
+++ app/posts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+class Post(models.Model):
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    photo = models.ImageField(upload_to='post', blank=True)
+
+    content = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
